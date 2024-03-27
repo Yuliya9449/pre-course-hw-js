66d0c0d386c04b54706256baf9ff87a92f48efa9
Yuliya Kotova <yulya9449@yandex.ru>
2024-03-27 03:20:40 +0300

    feat: complete lesson 1 task 7

---
+++ lesson-01/07.js
@@ -0,0 +1,7 @@
+//   Выведи в консоль «Я красавчик!».
+// ❗ Обрати внимание: «Я» с заглавной.
+
+const iAm = 'я';
+const krasavchik = 'красавчик!';
+
+console.log(`${iAm.toUpperCase()} ${krasavchik}`);
