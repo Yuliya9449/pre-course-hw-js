b36704bcc957011677ee8fb87046d225ac0fde12
Yuliya Kotova <yulya9449@yandex.ru>
2024-09-11 23:51:44 +0300

    fix: complete lesson 2 task 3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -5,4 +5,4 @@ const giveMeRightNumbers = (value1, value2, maximum) => {
     console.log('Something wrong!');
   }
 };
-giveMeRightNumbers(100, 200, 50);
+console.log(giveMeRightNumbers(100, 200, 50));
