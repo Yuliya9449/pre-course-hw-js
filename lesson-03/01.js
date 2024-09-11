6dbb98098590bb95e16356c66dc3a7b80ff9384f
Yuliya Kotova <yulya9449@yandex.ru>
2024-09-12 01:54:05 +0300

    feat: complete lesson 3 task 1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,9 @@
-// code
+const colors = ['синий', 'черный', 'зеленый', 'красный', 'желтый', 'зеленый'];
+
+function createColorString(first, second, third) {
+  return colors
+    .filter((color) => color === first || color === second || color === third)
+    .join('-');
+}
+
+console.log(createColorString('черный', 'красный', 'желтый'));
