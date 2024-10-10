37226c991d7e323026c6b827023a86843314ab69
Yuliya Kotova <yulya9449@yandex.ru>
2024-10-11 00:51:06 +0300

    refactor: lesson 2 task 6

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -1,3 +1,5 @@
+// Давайте-ка нашего парня, чтобы он не зазнавался переселим в ‘city’ 'Bobryisk' , а потом опять же убедимся в консоли, что мы все правильно скопировали. (Т.е. необходимо создать глубокую копию объекта passportWithAddress, а затем 'LA' заменить на 'Bobryisk' в city).
+
 let passportWithAddress = {
   name: 'Petr',
   surname: 'Petrov',
@@ -7,7 +9,9 @@ let passportWithAddress = {
   },
 };
 
-let copyPassportWithAddress = structuredClone(passportWithAddress);
+// let copyPassportWithAddress = structuredClone(passportWithAddress);
+let copyPassportWithAddress = {...passportWithAddress}
+copyPassportWithAddress.address = {...passportWithAddress.address}
 
 copyPassportWithAddress.address.city = 'Bobryisk';
 
