057c63982ecedb5dc5260c99546d006700c454d2
Yuliya Kotova <yulya9449@yandex.ru>
2024-10-11 00:45:59 +0300

    refactor: lesson 2 task 5

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -1,13 +1,11 @@
+// Дан объект passport. Создай его копию и поменяй имя с "Petr" на "Ivan", и убедись в консоли, что у объекта passport имя осталось "Petr", а у твоего нового объекта "Ivan".
+
 let passport = {
   name: 'Petr',
   surname: 'Petrov',
 };
 
-let copyPassport = {};
-
-for (let key in passport) {
-  copyPassport[key] = passport[key];
-}
+let copyPassport = { ...passport };
 
 copyPassport.name = 'Ivan';
 
